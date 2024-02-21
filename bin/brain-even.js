4e7ea5d78fb76ec5fe15fe5013a035076606fbed
Anton <antonblkv@yandex.ru>
2024-02-21 16:28:02 +0300

    add brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const parityCheck = (namePlayer) => {
+  let count = 0;
+  const answerOptions = ['yes', 'no'];
+
+  while (count !== 3) {
+    const randomNumber = Math.floor(Math.random() * 101);
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    const assumedParity = answerOptions.indexOf(answer);
+    const realParity = randomNumber % 2;
+
+    if (assumedParity === realParity) {
+      console.log('Correct!');
+      count += 1;
+    } else {
+      return `'${answer}' is wrong answer ;(. Correct answer was '${answerOptions[realParity]}'. Let's try again, ${namePlayer}!`;
+    }
+  }
+
+     return `Congratulations!, ${namePlayer}`;
+};
+
+console.log('node bin/brain-games.js.\nWelcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log(parityCheck(name));
