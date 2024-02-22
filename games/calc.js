ef6b4ba388290cd8f0c4322d52918b77a83cc633
Anton <antonblkv@yandex.ru>
2024-02-22 15:47:29 +0300

    made a separate implementation of games

---
+++ games/calc.js
@@ -0,0 +1,25 @@
+import {getNameAndWelcome, getActualResponse, getExpectedResponse, getExpression, getOperation, getRandomIntInclusive} from '../src/index.js';
+
+export default () => {
+  const name = getNameAndWelcome();
+  console.log('What is the result of the expression?');
+
+  for (let i = 0; i < 3; i += 1) {
+    const value1 = getRandomIntInclusive();
+    const value2 = getRandomIntInclusive();
+    const operation = getOperation(getRandomIntInclusive(0, 2));
+    const expression = getExpression(value1, value2, operation);
+
+    console.log(`Question: ${expression}`);
+    const expectedResponse = Number(getExpectedResponse());
+    const actualResponse = getActualResponse(value1, value2, operation);
+
+     if (expectedResponse === actualResponse) {
+      console.log('Correct!');
+    } else {
+      return `'${expectedResponse}' is wrong answer ;(. Correct answer was '${actualResponse}'. Let's try again, ${name}!`;
+    }
+  }
+
+  return `Congratulations!, ${name}`;
+};
