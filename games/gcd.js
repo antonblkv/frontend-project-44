8999f1fda9c0cfc46e1c6ef07e86299ae6727d51
Anton <antonblkv@yandex.ru>
2024-02-23 11:49:17 +0300

    add new game gcd

---
+++ games/gcd.js
@@ -0,0 +1,33 @@
+import {
+  getNameAndWelcome, getExpectedResponse,
+  getExpression, getRandomIntInclusive,
+} from '../src/index.js';
+
+const gcd = (value1, value2) => {
+  if (value2) return gcd(value2, value1 % value2);
+
+  return Math.abs(value1);
+};
+
+export default () => {
+  const name = getNameAndWelcome();
+  console.log('What is the result of the expression?');
+
+  for (let i = 0; i < 3; i += 1) {
+    const value1 = getRandomIntInclusive();
+    const value2 = getRandomIntInclusive();
+    const expression = getExpression(value1, value2);
+
+    console.log(`Question: ${expression}`);
+    const expectedResponse = Number(getExpectedResponse());
+    const actualResponse = gcd(value1, value2);
+
+    if (expectedResponse === actualResponse) {
+      console.log('Correct!');
+    } else {
+      return `'${expectedResponse}' is wrong answer ;(. Correct answer was '${actualResponse}'. Let's try again, ${name}!`;
+    }
+  }
+
+  return `Congratulations!, ${name}`;
+};
