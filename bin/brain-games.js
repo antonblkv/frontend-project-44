f8e4939fbd1ad1c26db311eb4fe1d37f840e26e4
Anton <antonblkv@yandex.ru>
2024-02-26 15:09:52 +0300

    fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import askName from '../src/cli.js';
+import getName from '../src/cli.js';
 
-console.log('node bin/brain-games.js.\nWelcome to the Brain Games!');
-askName();
+console.log('Welcome to the Brain Games!');
+console.log(getName());
