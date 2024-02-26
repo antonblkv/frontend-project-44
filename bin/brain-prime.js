f36145c7c66659234e51b698c0e9107539ee5c8f
Anton <antonblkv@yandex.ru>
2024-02-26 15:10:41 +0300

    add game prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import startGamePrime from '../games/prime.js';
+
+console.log(startGamePrime());
---
+++ games/prime.js
@@ -0,0 +1,36 @@
+import {
+  getNameAndWelcome, getExpectedResponse,
+  getExpression, getRandomIntInclusive,
+} from '../src/index.js';
+
+const isPrime = (value) => {
+  if (value < 2) return 'no';
+
+  for (let i = 2; i < value; i += 1) {
+    if (value % i === 0) return 'no';
+  }
+
+  return 'yes';
+};
+
+export default () => {
+  const name = getNameAndWelcome();
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const value = getRandomIntInclusive(0, 10);
+    const expression = getExpression(value);
+
+    console.log(`Question: ${expression}`);
+    const expectedResponse = getExpectedResponse();
+    const actualResponse = isPrime(value);
+
+    if (expectedResponse === actualResponse) {
+      console.log('Correct!');
+    } else {
+      return `'${expectedResponse}' is wrong answer ;(. Correct answer was '${actualResponse}'. Let's try again, ${name}!`;
+    }
+  }
+
+  return `Congratulations!, ${name}`;
+};
