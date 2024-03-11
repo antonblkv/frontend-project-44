8281847994c7186e7694e2024e2ede7d9dfd7b2d
Anton <antonblkv@yandex.ru>
2024-03-11 11:51:19 +0300

    fix func isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,13 +2,13 @@ import gameEngine from '../index.js';
 import getRandomIntInclusive from '../helper.js';
 
 const isPrime = (value) => {
-  if (value < 2) return 'no';
+  if (value < 2) return false;
 
   for (let i = 2; i < value; i += 1) {
-    if (value % i === 0) return 'no';
+    if (value % i === 0) return false;
   }
 
-  return 'yes';
+  return true;
 };
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
@@ -16,9 +16,9 @@ const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer
 const getGameData = () => {
   const number = getRandomIntInclusive();
 
-  const correctResponse = isPrime(number);
+  const correctResponse = isPrime(number) ? 'yes' : 'no';
 
   return [number, correctResponse];
 };
 
-export default () => console.log(gameEngine(gameDescription, getGameData));
+export default () => gameEngine(gameDescription, getGameData);
